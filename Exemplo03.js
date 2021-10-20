7c055f85fc6d835e4342a80e1c0ed5c62b2dde81
Gustavo Molina <70485830+gustavomolina17@users.noreply.github.com>
2021-10-20 12:51:17 -0300

    Adicionando Exemplo 03

---
+++ Exemplo03.js
@@ -0,0 +1,26 @@
+/*
+
+Esse exemplo demonstra operações com arrays
+
+*/
+
+const lista = [1, 2, 3, 4, 5, 6];
+
+const novaLista = lista.map(function (item, index) {
+  // return item *5; - Multiplica todos os elementos por 5
+  return item + index;
+});
+
+console.log(novaLista);
+
+const soma = lista.reduce(function (total, proximo) {
+  return total + proximo;
+});
+
+console.log(soma);
+
+const find = lista.find(function (item) {
+  return item === 6;
+});
+
+console.log(find);
